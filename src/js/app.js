52bb67169e803a53b9f285164e34c6ba56a8306e
Adrian <adicirstei@gmail.com>
2013-08-01 10:18:41 +0300

    replaced multifile box2d with box2dweb

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+define(['Box2D'], function(Box2D){
+  console.log(Box2D);
+  return {Box2D: Box2D};
+
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
 require.config({
-
+  shim: {
+    'Box2D': {
+      exports: 'Box2D'
+    }
+  },
+  paths: {
+    'Box2D': '../components/box2dweb/Box2dWeb-2.1.a.3'
+  }
 });
 
 require(['app'], function (app) {
