67527b88a00fae9da72645d7feb1ebc96dbe66d3
adrian <adicirstei@gmail.com>
2013-08-01 23:59:59 +0300

    world

--- src/js/Game.js
+++ src/js/Game.js
@@ -3,6 +3,12 @@ define(['World'], function(World){
   return {
     start: function(){
       var w = new World();
+    },
+    render:function(){
+      requestAnimationFrame(this.render);
+      // render code goes here
+
+      
     }
   };
 
