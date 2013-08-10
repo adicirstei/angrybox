3267f8a97699d3cfb66592964ae20a12d0a6fa2d
adrian <adicirstei@gmail.com>
2013-08-11 01:43:25 +0300

    added createjs libraries

--- src/js/main.js
+++ src/js/main.js
@@ -7,18 +7,32 @@ require.config({
     'boxbox' :{
       exports: 'boxbox',
       deps: ['Box2D'],
-    }
+    },
+    'easeljs': {
+      exports: 'createjs'
+    },
+    'soundjs': {
+      exports: 'createjs'
+    },
+    'preloadjs': {
+      exports: 'createjs'
+    }    
   },
   paths: {
     'Box2D': '../components/boxgame/Box2dWeb-2.1.a.3',
-    'boxbox': '../components/boxgame/boxbox'
+    'boxbox': '../components/boxgame/boxbox',
+    'easeljs': '../components/easeljs/index',
+    'soundjs': '../components/soundjs/index',
+    'preloadjs': '../components/preloadjs/index'
   }
 });
 
 
-require(['app'], function (app) {
+require(['app', 'preloadjs'], function (app, easeljs) {
     'use strict';
     
     console.log(app);
+    
+    console.log(easeljs);
     
 });
