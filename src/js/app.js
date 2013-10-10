bd2c7fc663295b3d44e7bf9f67a9d6abcf1ae8fb
Adrian <adicirstei@gmail.com>
2013-10-10 14:41:26 +0300

    added some assets

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,31 @@
 define(['core', 'Game'], function(ab, Game){
   window.AngryBox = ab;
 
+  // set up the drawing context
   ab.gameCanvas = document.getElementById('canvas');
   ab.context = ab.gameCanvas.getContext('2d');
+
+  ab.viewport = {x:0, y: 0, w: 800, h: 600};
+
+  ab.gameCanvas.width = ab.viewport.w;
+  ab.gameCanvas.height = ab.viewport.h;
+
+
+
+  // draw start screen
+  // 1. draw the splash image
+  // 2. draw fake progress image
+
+
+
+
+
+  // start loading the settings
+
+
+
+
+
 
   // ab.game = game;
   // game.start();
