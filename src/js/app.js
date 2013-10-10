e5cab3c79a2dfe509945fbc72685dbe3725e74a1
Adrian <adicirstei@gmail.com>
2013-10-10 15:19:29 +0300

    progressing

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,17 @@
 define(['core', 'Game'], function(ab, Game){
   window.AngryBox = ab;
 
+  var drawSplash = function(ctx){
+    var img = new Image();
+    img.onload = function(){
+      ctx.drawImage(this, 0, 0);
+    };
+    img.src = "../img/splash.png";
+  };
+  var loadMenu = function(settings) {
+    console.log(settings);
+  };
+
   // set up the drawing context
   ab.gameCanvas = document.getElementById('canvas');
   ab.context = ab.gameCanvas.getContext('2d');
@@ -9,19 +20,20 @@ define(['core', 'Game'], function(ab, Game){
 
   ab.gameCanvas.width = ab.viewport.w;
   ab.gameCanvas.height = ab.viewport.h;
-
+  
 
 
   // draw start screen
   // 1. draw the splash image
   // 2. draw fake progress image
-
-
-
+  drawSplash(ab.context);
 
 
   // start loading the settings
-
+  ab.xhrGet('../assets/user.json', function(data){
+    var s = JSON.parse(this.responseText);
+    loadMenu(s);
+  });
 
 
 
@@ -29,4 +41,5 @@ define(['core', 'Game'], function(ab, Game){
 
   // ab.game = game;
   // game.start();
+
 });
